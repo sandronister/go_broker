19293d6a526720da75ee3a51325dc34c95657336
Julio Sandroni <sandronister@gmail.com>
2024-04-24 14:48:57 -0300

    feat:consumer kafka

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,33 @@ import (
 	"github.com/sandronister/go-broker/pkg/kafka"
 )
 
+func printMessage(message <-chan pkg.ReceiptMessage) {
+	for msg := range message {
+		println("Message Received:")
+		println("TopicPartition: ", msg.TopicPartition)
+		println("Value: ", string(msg.Value))
+		println("Key: ", string(msg.Key))
+
+		for _, h := range msg.Headers {
+			println("Headers: ", h)
+		}
+	}
+}
+
 func main() {
 
 	broker := kafka.NewBroker("localhost", 9092)
 
-	message := &pkg.Message{
-		TopicPartition: "myTopic",
-		Value:          "New Producer Hello Go!",
-		Headers:        "header values are binary",
+	message := make(chan pkg.ReceiptMessage)
+
+	for i := 0; i < 10; i++ {
+		go printMessage(message)
 	}
 
-	broker.Produce(message)
+	err := broker.Consume("myTopic", message)
+
+	if err != nil {
+		println(err)
+	}
 
 }
