e461f10a293e4301d7730b951883c7da9d10b1eb
Julio Sandroni <julio.sandroni@gobrax.com>
2024-09-06 15:36:01 -0300

    fix:message marshalling

--- pkg/broker/broker_redis/producer.go
+++ pkg/broker/broker_redis/producer.go
@@ -2,10 +2,15 @@ package brokerredis
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/sandronister/go_broker/pkg/broker/types"
 )
 
 func (b *Broker) Producer(message *types.Message) error {
-	return b.client.LPush(context.Background(), message.Topic, message).Err()
+	messageByte, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+	return b.client.LPush(context.Background(), message.Topic, messageByte).Err()
 }
